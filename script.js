ca49a07f6661cc27d802a9f9cbb67e6bd384632b
amlano <154421776+amlano@users.noreply.github.com>
2026-06-16 12:00:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 50){
+
+        header.style.boxShadow =
+        "0 2px 10px rgba(0,0,0,.2)";
+
+    }else{
+
+        header.style.boxShadow =
+        "none";
+
+    }
+
+});
